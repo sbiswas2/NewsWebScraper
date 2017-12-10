0ea1888767b2856a7ecac46f8541b4cc53d7dcc5
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-12-10 15:35:53 -0600

    folder structure set up

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+var express = require("express");
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var request = require('request');
+var cheerio = require('cheerio');
+
